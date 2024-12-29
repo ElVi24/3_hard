64faf43bd7789a7e5f7c65d6cd1b182134899044
ElVi24 <elenavicher@gmail.com>
2024-12-30 01:28:30 +0700

    Update module_3_hard.py

--- module_3_hard.py
+++ module_3_hard.py
@@ -6,17 +6,17 @@ data_structure = [
   ((), [{(2, 'Urban', ('Urban2', 35))}])
 ]
 def calculate_structure_sum (data_structure):
-    summa = 0
+    sum = 0
     if isinstance(data_structure, dict):
         for args in data_structure.items():
-            summa += calculate_structure_sum(args)
+            sum += calculate_structure_sum(args)
     elif isinstance(data_structure, (list, tuple, set)):
         for args in data_structure:
-            summa += calculate_structure_sum(args)
+            sum += calculate_structure_sum(args)
     elif isinstance(data_structure, (int, float)):
-        summa += data_structure
+        sum += data_structure
     elif isinstance(data_structure, str):
-        summa += len(data_structure)
-    return summa
+        sum += len(data_structure)
+    return sum
 result = calculate_structure_sum(data_structure)
 print(result)
